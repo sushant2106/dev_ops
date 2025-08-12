93b42b17482d22eda7c468b179b91b4eb6554c54
sushant gupta <sushantgupta4u@gmail.com>
2025-08-12 12:53:43 +0530

    adding

---
+++ demo.py
@@ -0,0 +1,15 @@
+# def my_function():
+#     a=5
+#     b=6
+
+#     return d
+# my_function()
+
+def funct():
+    """ this is fucntion """
+    print('tsting')
+
+
+
+if __name__=='__main__':
+    funct()
--- testing.py
+++ testing.py
@@ -1,1 +1,6 @@
 print("hello world")
+
+
+
+if __name__=='__main__':
+    pass
